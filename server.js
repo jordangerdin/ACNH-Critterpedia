4cc7e9c82059734715cc7a06b3a209cc9c93a9b4
Jordan Gerdin <jordan.gerdin@gmail.com>
2020-05-13 23:34:51 -0600

    Heroku deployment

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 let express = require('express')
 let routes = require('./routes/routes')
+let path = require('path')
 
 // App configuration
 let app = express()
+
+app.use(express.static(path.join(__dirname, 'acnh-client', 'dist')))
+
+app.use('/', routes)
 
 // Start server running
 let server = app.listen(process.env.PORT || 3000, function() {
